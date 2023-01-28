daff49918975681d2b9ab7860cdb61b25d49c4e2
MingzhiXuWill <mx2091@nyu.edu>
2023-01-27 20:14:23 -0500

    01/27/2023 / Room editing: place & remove & different type / WIP: work with inventory UI

---
+++ Assets/Will Temp/GridBuildingSystem2D.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridBuildingSystem2D : MonoBehaviour {
+
+    public static GridBuildingSystem2D Instance { get; private set; }
+
+    public event EventHandler OnSelectedChanged;
+    public event EventHandler OnObjectPlaced;
+
+    public static int sortingOrderDefault = 5000;
+
+    private Grid<GridObject> grid;
+
+    [Header("Grid")]
+    public int gridWidth = 5;
+    public int gridHeight = 5;
+    public float cellSize = 3f;
+    public bool debugMode = true;
+
+    [SerializeField] private List<PlacedObjectTypeSO> placedObjectTypeSOList = null;
+    private PlacedObjectTypeSO placedObjectTypeSO;
+    private PlacedObjectTypeSO.Dir dir;
+
+    private void Awake() {
+        Instance = this;
+
+        grid = new Grid<GridObject>(gridWidth, gridHeight, cellSize, new Vector3(0, 0, 0), (Grid<GridObject> g, int x, int y) => new GridObject(g, x, y), debugMode);
+
+        placedObjectTypeSO = null;
+    }
+
+    public class GridObject {
+
+        private Grid<GridObject> grid;
+        private int x;
+        private int y;
+        public PlacedObject_Done placedObject;
+
+        public GridObject(Grid<GridObject> grid, int x, int y) {
+            this.grid = grid;
+            this.x = x;
+            this.y = y;
+            placedObject = null;
+        }
+
+        public override string ToString() {
+            return x + ", " + y + "\n" + placedObject;
+        }
+
+        public void SetPlacedObject(PlacedObject_Done placedObject) {
+            this.placedObject = placedObject;
+            grid.TriggerGridObjectChanged(x, y);
+        }
+
+        public void ClearPlacedObject() {
+            placedObject = null;
+            grid.TriggerGridObjectChanged(x, y);
+        }
+
+        public PlacedObject_Done GetPlacedObject() {
+            return placedObject;
+        }
+
+        public bool CanBuild() {
+            return placedObject == null;
+        }
+
+    }
+
+    private void Update() {
+        if (Input.GetMouseButtonDown(0) && placedObjectTypeSO != null) {
+            Debug.Log("clicked" + placedObjectTypeSO);
+
+            Vector3 mousePosition = GetMouseWorldPosition();
+            grid.GetXY(mousePosition, out int x, out int z);
+
+            Vector2Int placedObjectOrigin = new Vector2Int(x, z);
+
+            // Test Can Build
+            List<Vector2Int> gridPositionList = placedObjectTypeSO.GetGridPositionList(placedObjectOrigin, dir);
+            bool canBuild = true;
+            foreach (Vector2Int gridPosition in gridPositionList) {
+                if (!grid.GetGridObject(gridPosition.x, gridPosition.y).CanBuild()) {
+                    canBuild = false;
+                    break;
+                }
+            }
+
+            if (canBuild) {
+                Vector2Int rotationOffset = placedObjectTypeSO.GetRotationOffset(dir);
+                Vector3 placedObjectWorldPosition = grid.GetWorldPosition(x, z) + new Vector3(rotationOffset.x, rotationOffset.y) * grid.GetCellSize();
+
+                PlacedObject_Done placedObject = PlacedObject_Done.Create(placedObjectWorldPosition, placedObjectOrigin, dir, placedObjectTypeSO);
+                placedObject.transform.rotation = Quaternion.Euler(0, 0, -placedObjectTypeSO.GetRotationAngle(dir));
+
+                foreach (Vector2Int gridPosition in gridPositionList) {
+                    grid.GetGridObject(gridPosition.x, gridPosition.y).SetPlacedObject(placedObject);
+                }
+
+                OnObjectPlaced?.Invoke(this, EventArgs.Empty);
+
+                //DeselectObjectType();
+            } else {
+                // Cannot build here
+                // CreateWorldTextPopup("Cannot Build Here!", mousePosition);
+                Debug.Log("Cant build here");
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.R)) {
+            dir = PlacedObjectTypeSO.GetNextDir(dir);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha1)) { placedObjectTypeSO = placedObjectTypeSOList[0]; RefreshSelectedObjectType(); }
+        if (Input.GetKeyDown(KeyCode.Alpha2)) { placedObjectTypeSO = placedObjectTypeSOList[1]; RefreshSelectedObjectType(); }
+        if (Input.GetKeyDown(KeyCode.Alpha3)) { placedObjectTypeSO = placedObjectTypeSOList[2]; RefreshSelectedObjectType(); }
+        if (Input.GetKeyDown(KeyCode.Alpha4)) { placedObjectTypeSO = placedObjectTypeSOList[3]; RefreshSelectedObjectType(); }
+        if (Input.GetKeyDown(KeyCode.Alpha5)) { placedObjectTypeSO = placedObjectTypeSOList[4]; RefreshSelectedObjectType(); }
+        if (Input.GetKeyDown(KeyCode.Alpha6)) { placedObjectTypeSO = placedObjectTypeSOList[5]; RefreshSelectedObjectType(); }
+
+        if (Input.GetKeyDown(KeyCode.Alpha0)) { DeselectObjectType(); }
+
+
+        if (Input.GetMouseButtonDown(1)) {
+            Vector3 mousePosition = GetMouseWorldPosition();
+            PlacedObject_Done placedObject = grid.GetGridObject(mousePosition).GetPlacedObject();
+            if (placedObject != null) {
+                // Demolish
+                placedObject.DestroySelf();
+
+                List<Vector2Int> gridPositionList = placedObject.GetGridPositionList();
+                foreach (Vector2Int gridPosition in gridPositionList) {
+                    grid.GetGridObject(gridPosition.x, gridPosition.y).ClearPlacedObject();
+                }
+            }
+        }
+    }
+
+    private void DeselectObjectType() {
+        placedObjectTypeSO = null; RefreshSelectedObjectType();
+    }
+
+    private void RefreshSelectedObjectType() {
+        OnSelectedChanged?.Invoke(this, EventArgs.Empty);
+    }
+
+
+    public Vector2Int GetGridPosition(Vector3 worldPosition) {
+        grid.GetXY(worldPosition, out int x, out int z);
+        return new Vector2Int(x, z);
+    }
+
+    public Vector3 GetMouseWorldSnappedPosition() {
+        Vector3 mousePosition = GetMouseWorldPosition();
+        grid.GetXY(mousePosition, out int x, out int y);
+
+        if (placedObjectTypeSO != null) {
+            Vector2Int rotationOffset = placedObjectTypeSO.GetRotationOffset(dir);
+            Vector3 placedObjectWorldPosition = grid.GetWorldPosition(x, y) + new Vector3(rotationOffset.x, rotationOffset.y) * grid.GetCellSize();
+            return placedObjectWorldPosition;
+        } else {
+            return mousePosition;
+        }
+    }
+
+    public Quaternion GetPlacedObjectRotation() {
+        if (placedObjectTypeSO != null) {
+            return Quaternion.Euler(0, 0, -placedObjectTypeSO.GetRotationAngle(dir));
+        } else {
+            return Quaternion.identity;
+        }
+    }
+
+    public PlacedObjectTypeSO GetPlacedObjectTypeSO() {
+        return placedObjectTypeSO;
+    }
+
+    public static Vector3 GetMouseWorldPosition()
+    {
+        Vector3 vec = GetMouseWorldPositionWithZ(Input.mousePosition, Camera.main);
+        vec.z = 0f;
+        return vec;
+    }
+
+    public static Vector3 GetMouseWorldPositionWithZ()
+    {
+        return GetMouseWorldPositionWithZ(Input.mousePosition, Camera.main);
+    }
+
+    public static Vector3 GetMouseWorldPositionWithZ(Camera worldCamera)
+    {
+        return GetMouseWorldPositionWithZ(Input.mousePosition, worldCamera);
+    }
+
+    public static Vector3 GetMouseWorldPositionWithZ(Vector3 screenPosition, Camera worldCamera)
+    {
+        Vector3 worldPosition = worldCamera.ScreenToWorldPoint(screenPosition);
+        return worldPosition;
+    }
+
+    public static void CreateWorldTextPopup(string text, Vector3 localPosition, float popupTime = 1f)
+    {
+        CreateWorldTextPopup(null, text, localPosition, 40, Color.white, localPosition + new Vector3(0, 20), popupTime);
+    }
+
+    // Create a Text Popup in the World
+    public static void CreateWorldTextPopup(Transform parent, string text, Vector3 localPosition, int fontSize, Color color, Vector3 finalPopupPosition, float popupTime)
+    {
+        TextMesh textMesh = CreateWorldText(parent, text, localPosition, fontSize, color, TextAnchor.LowerLeft, TextAlignment.Left, sortingOrderDefault);
+        Transform transform = textMesh.transform;
+        Vector3 moveAmount = (finalPopupPosition - localPosition) / popupTime;
+        /*
+        FunctionUpdater.Create(delegate () {
+            transform.position += moveAmount * Time.unscaledDeltaTime;
+            popupTime -= Time.unscaledDeltaTime;
+            if (popupTime <= 0f)
+            {
+                UnityEngine.Object.Destroy(transform.gameObject);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }, "WorldTextPopup");
+        */
+    }
+
+    public static TextMesh CreateWorldText(string text, Transform parent = null, Vector3 localPosition = default(Vector3), int fontSize = 40, Color? color = null, TextAnchor textAnchor = TextAnchor.UpperLeft, TextAlignment textAlignment = TextAlignment.Left, int sortingOrder = 5000)
+    {
+        if (color == null) color = Color.white;
+        return CreateWorldText(parent, text, localPosition, fontSize, (Color)color, textAnchor, textAlignment, sortingOrder);
+    }
+   
+    public static TextMesh CreateWorldText(Transform parent, string text, Vector3 localPosition, int fontSize, Color color, TextAnchor textAnchor, TextAlignment textAlignment, int sortingOrder)
+    {
+        GameObject gameObject = new GameObject("World_Text", typeof(TextMesh));
+        Transform transform = gameObject.transform;
+        transform.SetParent(parent, false);
+        transform.localPosition = localPosition;
+        TextMesh textMesh = gameObject.GetComponent<TextMesh>();
+        textMesh.anchor = textAnchor;
+        textMesh.alignment = textAlignment;
+        textMesh.text = text;
+        textMesh.fontSize = fontSize;
+        textMesh.color = color;
+        textMesh.GetComponent<MeshRenderer>().sortingOrder = sortingOrder;
+        return textMesh;
+    }
+}
